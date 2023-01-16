0ad7ff4852a531a07c3184b7dce86c157ba96c7d
Henrique Sato <henriquesato2003@gmail.com>
2023-01-16 15:10:37 -0300

    All entities repositories

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -14,6 +14,7 @@ import java.util.Set;
 public class Enrollment implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
     @EmbeddedId
     private EnrollmentPK id = new EnrollmentPK();
 
